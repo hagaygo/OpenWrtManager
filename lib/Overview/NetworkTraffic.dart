483c8f1ebfe4712684c547a45288515a87478b88
Hagay Goshen <hagay.goshen@gmail.com>
2022-08-23 18:50:58 +0300

    remove font sizes to fix some issues on "large" display devices

--- lib/Overview/NetworkTraffic.dart
+++ lib/Overview/NetworkTraffic.dart
@@ -12,9 +12,16 @@ import 'package:openwrt_manager/my_flutter_app_icons.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 
 class NetworkTraffic extends OverviewWidgetBase {
-  NetworkTraffic(Device device, bool loading, AuthenticateReply authenticationStatus, List<CommandReplyBase> replies,
-      OverviewItem item, String overviewItemGuid, Function doOverviewRefresh)
-      : super(device, loading, authenticationStatus, replies, item, overviewItemGuid, doOverviewRefresh);
+  NetworkTraffic(
+      Device device,
+      bool loading,
+      AuthenticateReply authenticationStatus,
+      List<CommandReplyBase> replies,
+      OverviewItem item,
+      String overviewItemGuid,
+      Function doOverviewRefresh)
+      : super(device, loading, authenticationStatus, replies, item,
+            overviewItemGuid, doOverviewRefresh);
 
   @override
   State<StatefulWidget> createState() {
@@ -39,41 +46,56 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
         _interfaces.add(name);
         if (configData != null && configData[name] != true) continue;
         if (ifCounter > 1) rows.add(SizedBox(height: 10));
-        var incoming = double.parse(iff["stats"]["rx_bytes"].toString()).toInt();
-        var outgoing = double.parse(iff["stats"]["tx_bytes"].toString()).toInt();
+        var incoming =
+            double.parse(iff["stats"]["rx_bytes"].toString()).toInt();
+        var outgoing =
+            double.parse(iff["stats"]["tx_bytes"].toString()).toInt();
 
         if (_trafficData[name] != null) {
           var incomingDiff = incoming - _trafficData[name]["in"];
           var outgoingDiff = outgoing - _trafficData[name]["out"];
           if (gotNewData) {
             var currentTimeStamp = new DateTime.now().millisecondsSinceEpoch;
-            var timeDiff = (currentTimeStamp - _trafficData[name]["timeStamp"]) / 1000;
+            var timeDiff =
+                (currentTimeStamp - _trafficData[name]["timeStamp"]) / 1000;
             _trafficData[name]["timeStamp"] = currentTimeStamp;
-            _trafficData[name]["inSpeed"] = Utils.formatBytes((incomingDiff / timeDiff).round(), decimals: 1);
-            _trafficData[name]["outSpeed"] = Utils.formatBytes((outgoingDiff / timeDiff).round(), decimals: 1);
+            _trafficData[name]["inSpeed"] = Utils.formatBytes(
+                (incomingDiff / timeDiff).round(),
+                decimals: 1);
+            _trafficData[name]["outSpeed"] = Utils.formatBytes(
+                (outgoingDiff / timeDiff).round(),
+                decimals: 1);
           }
         }
 
         String incomingSpeed = " " + Utils.NoSpeedCalculationText + " Kb/s";
         String outgoingSpeed = " " + Utils.NoSpeedCalculationText + " Kb/s";
 
-        if (_trafficData[name] != null && _trafficData[name]["inSpeed"] != null) {
+        if (_trafficData[name] != null &&
+            _trafficData[name]["inSpeed"] != null) {
           incomingSpeed = "${_trafficData[name]["inSpeed"]}/s";
           outgoingSpeed = "${_trafficData[name]["outSpeed"]}/s";
         }
 
-        var interface =
-            interfaces.firstWhere((x) => x["l3_device"] == iff["name"], orElse: () => null); // first try by name
+        var interface = interfaces.firstWhere(
+            (x) => x["l3_device"] == iff["name"],
+            orElse: () => null); // first try by name
 
         if (interface == null) // then try to match by master device
-          interface = interfaces.firstWhere((x) => iff["master"] == x["l3_device"], orElse: () => null);
+          interface = interfaces.firstWhere(
+              (x) => iff["master"] == x["l3_device"],
+              orElse: () => null);
 
         String uptime = "";
         String interfaceAddress = "";
         if (interface != null) {
-          if (interface["uptime"] != null) uptime = Utils.formatDuration(Duration(seconds: interface["uptime"]));
-          if (interface["ipv4-address"] != null && (interface["ipv4-address"] as List).length > 0)
-            interfaceAddress = "${interface["ipv4-address"][0]["address"] ?? ""}";
+          if (interface["uptime"] != null)
+            uptime =
+                Utils.formatDuration(Duration(seconds: interface["uptime"]));
+          if (interface["ipv4-address"] != null &&
+              (interface["ipv4-address"] as List).length > 0)
+            interfaceAddress =
+                "${interface["ipv4-address"][0]["address"] ?? ""}";
         }
 
         var interfaceHeaderRow = Row(
@@ -106,12 +128,14 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
             Expanded(
               child: Align(
                   alignment: Alignment.center,
-                  child: getTrafficWidgetBytes(incoming, MyFlutterApp.down_bold, incomingSpeed)),
+                  child: getTrafficWidgetBytes(
+                      incoming, MyFlutterApp.down_bold, incomingSpeed)),
             ),
             Expanded(
               child: Align(
                   alignment: Alignment.center,
-                  child: getTrafficWidgetBytes(outgoing, MyFlutterApp.up_bold, outgoingSpeed)),
+                  child: getTrafficWidgetBytes(
+                      outgoing, MyFlutterApp.up_bold, outgoingSpeed)),
             )
           ],
         );
@@ -119,7 +143,8 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
         var interfaceBox = Container(
             padding: EdgeInsets.all(3),
             child: InkWell(
-                onLongPress: () async => { await showRestartInterfaceOptionsDialog(interface)},
+                onLongPress: () async =>
+                    {await showRestartInterfaceOptionsDialog(interface)},
                 child: Column(
                   children: <Widget>[
                     interfaceHeaderRow,
@@ -133,11 +158,13 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
                   ],
                 )));
         rows.add(interfaceBox);
-        if (_trafficData[name] == null) _trafficData[name] = Map<String, dynamic>();
+        if (_trafficData[name] == null)
+          _trafficData[name] = Map<String, dynamic>();
         _trafficData[name]["out"] = outgoing;
         _trafficData[name]["in"] = incoming;
         if (_trafficData[name]["timeStamp"] == null)
-          _trafficData[name]["timeStamp"] = new DateTime.now().millisecondsSinceEpoch;
+          _trafficData[name]["timeStamp"] =
+              new DateTime.now().millisecondsSinceEpoch;
         ifCounter++;
       }
     }
@@ -153,7 +180,9 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
       children: <Widget>[
         Container(
             width: 75,
-            child: Align(alignment: Alignment.centerRight, child: Text("${Utils.formatBytes(bytes, decimals: 1)}"))),
+            child: Align(
+                alignment: Alignment.centerRight,
+                child: Text("${Utils.formatBytes(bytes, decimals: 1)}"))),
         SizedBox(width: 2),
         Icon(ico, size: 12),
         SizedBox(width: 2),
@@ -165,7 +194,13 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
   @override
   List<Map<String, dynamic>> get configItems {
     if (_interfaces == null || _interfaces.length == 0) return null;
-    return _interfaces.map((x) => {"name": "$x", "type": "bool", "category": "Select interfaces to show"}).toList();
+    return _interfaces
+        .map((x) => {
+              "name": "$x",
+              "type": "bool",
+              "category": "Select interfaces to show"
+            })
+        .toList();
   }
 
   @override
@@ -173,34 +208,40 @@ class NetworkTrafficState extends OverviewWidgetBaseState {
     return true;
   }
 
-  Future showRestartInterfaceOptionsDialog(interface)  async {
-    Alert(context: context, title: "${interface["interface"]} (${interface["device"]})", desc: "" , buttons: [
-      DialogButton(
-        color: Colors.red,
-        child: Text(
-          "Restart",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        onPressed: () async {
-          var client = OpenWRTClient(widget.device, null);
-          Dialogs.showLoadingDialog(context);
-          var res = await client.restartInterface(widget.authenticationStatus, interface["interface"]);
-          if (res.status != ReplyStatus.Ok) {
-             Dialogs.simpleAlert(context, "Error", "Interface restart request returned error");
-           } 
-          Navigator.pop(context);
-          Navigator.pop(context);
-        },
-      ),
-      DialogButton(
-        child: Text(
-          "Close",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        ),
-        onPressed: () {
-          Navigator.pop(context);
-        },
-      )
-    ]).show();
+  Future showRestartInterfaceOptionsDialog(interface) async {
+    Alert(
+        context: context,
+        title: "${interface["interface"]} (${interface["device"]})",
+        desc: "",
+        buttons: [
+          DialogButton(
+            color: Colors.red,
+            child: Text(
+              "Restart",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              var client = OpenWRTClient(widget.device, null);
+              Dialogs.showLoadingDialog(context);
+              var res = await client.restartInterface(
+                  widget.authenticationStatus, interface["interface"]);
+              if (res.status != ReplyStatus.Ok) {
+                Dialogs.simpleAlert(context, "Error",
+                    "Interface restart request returned error");
+              }
+              Navigator.pop(context);
+              Navigator.pop(context);
+            },
+          ),
+          DialogButton(
+            child: Text(
+              "Close",
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ]).show();
   }
 }
