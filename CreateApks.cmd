2976146a100ad10a1188cdb20f017895af507f01
Hagay Goshen <hagay.goshen@gmail.com>
2026-06-20 19:24:18 +0300

    Change build scripts to compress for apk and not compress on bundle https://github.com/hagaygo/OpenWrtManager/issues/81

--- CreateApks.cmd
+++ CreateApks.cmd
@@ -1,3 +1,3 @@
 call flutter clean
-call flutter build apk --release  --target-platform android-arm,android-arm64
+call flutter build apk --release  --target-platform android-arm,android-arm64 --dart-define=COMPRESS_APK=true
 move build\app\outputs\apk\release\app-release.apk build\app\outputs\apk\release\openwrt_manager.apk
